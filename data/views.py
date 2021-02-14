d46ec951ee4198d8b3cf792072ab5f61bc5687ab
Manoj Adhikari <53685400+jonamadk@users.noreply.github.com>
2021-02-14 13:07:03 +0545

    Add Token Authentication

--- data/views.py
+++ data/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .scrape import scrape_data
 from .models import LiveTradingData
 from .serializers import LiveTradingDataSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication, TokenAuthentication, SessionAuthentication
 
 from rest_framework import generics
 
@@ -14,6 +16,8 @@ class LiveTradingDataList(generics.ListCreateAPIView):
     
     queryset = LiveTradingData.objects.all()
     serializer_class = LiveTradingDataSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
   
 
     
@@ -23,5 +27,7 @@ class IndividualLiveTradingDataDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = LiveTradingData.objects.all()
     serializer_class = LiveTradingDataSerializer
+    authentication_classes =[TokenAuthentication]
+    permission_classes = [IsAuthenticated]
   
 
